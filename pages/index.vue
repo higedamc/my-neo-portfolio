137a3023a3fc892c382978d44bc07612346a8829
HigeMan <jstache@tutanota.com>
2021-07-14 22:34:18 +0900

    About Me 変更

--- pages/index.vue
+++ pages/index.vue
@@ -116,8 +116,7 @@
         <section class="bg-gray-800 pattern py-20">
             <div class="max-w-5xl px-6 mx-auto text-center">
                 <h2 class="text-2xl font-semibold text-white">About Me</h2>
-
-                <p class="text-gray-400 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper nulla nunc quis molestie volutpat elementum at. Ultrices ipsum, enim cursus lorem ac. Orci maecenas praesent arcu eget orci est orci nullam. Leo purus est pellentesque massa at tortor, est. Aliquet pulvinar a mattis sagittis. Suspendisse porta id elementum, massa.</p>
+                <p class="text-gray-400 mt-4">起業家 / 通・翻訳・発音矯正家 / Cypherpunk/ 元CoinTelegraph Japan Editing & Publishing Lead / BTC & WireGuard狂 / 元シーシャ屋経営者</p>
             </div>
         </section>
 
