f05b2660752539f35bb678be80c69880333742fe
HigeMan <kohei.otani@tutanota.com>
2023-10-28 19:24:26 +0900

    Fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -188,7 +188,7 @@
                   class="mt-8 max-w-2xl w-full block bg-white shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                   <div class="flex items-center justify-between px-4 py-2">
                     <h3 class="text-lg font-medium text-gray-700">
-                      Freelane Flutter Developer
+                      Freelance Flutter Developer
                     </h3>
                     <span class="block text-gray-600 font-light text-sm">Since 2020 - Present</span>
                   </div>
@@ -198,7 +198,7 @@
                   class="mt-8 max-w-2xl w-full block bg-white shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                   <div class="flex items-center justify-between px-4 py-2">
                     <h3 class="text-lg font-medium text-gray-700">
-                      Freelane Translator / Interpreter
+                      Freelance Translator / Interpreter
                     </h3>
                     <span class="block text-gray-600 font-light text-sm">Since 2013 - Present</span>
                   </div>
